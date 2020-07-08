07faf345f4bde439e1fc159d5c7b3eaeebd8ff6d
Levi Preto <levi.preto@gmail.com>
2020-07-08 16:49:46 -0300

    commit on master

--- Misc/PrimeNumber.java
+++ Misc/PrimeNumber.java
@@ -1,34 +1,38 @@
-// WORK IN PROGRESS
 package Misc;
 
 public class PrimeNumber {
-   
-    static int primeNumber = 1;
-    static boolean isPrime = true;
-    static int x;
-    static int loop = 1;
 
     public static void main(String[] args) {
 
-        while (primeNumber < 100) {
+        int i;
+        int maxCheck = 100; // limit to check
+        boolean isPrime = true;
 
-            primeNumber = primeNumber + 1;
+        // Empty String
+        String primeNumbersFound = "";
 
-            x = primeNumber / primeNumber;
-            
-            while (loop < primeNumber) {
-
-                x = primeNumber / loop;
-
-                loop = loop + 1;
-
-                if (x == 1 || x == 0)
-
-                    isPrime = false;
-
-            if (isPrime)
-            System.out.println("Number " + primeNumber + " is prime");
+        // Start loop 1 to maxCheck
+        for (i = 1; i <= maxCheck; i++) {
+            isPrime = CheckPrime(i);
+            if (isPrime) {
+                primeNumbersFound = primeNumbersFound + i + " ";
             }
         }
+        System.out.println("Prime numbers from 1 to " + maxCheck + " are:");
+        // Print prime numbers from 1 to maxCheck
+        System.out.println(primeNumbersFound);
     }
-}   
+
+    public static boolean CheckPrime(int numberToCheck) {
+        int remainder;
+        for (int i = 2; i <= numberToCheck / 2; i++) {
+            remainder = numberToCheck % i;
+            // if the remainder is 0 then numberToCheck is not prime and break loop. Else
+            // continue loop
+            if (remainder == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

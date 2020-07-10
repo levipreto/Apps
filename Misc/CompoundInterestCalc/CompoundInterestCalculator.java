4eb7d1976b455594c82e5194a57ba2b02b7fefd6
Levi Preto <levi.preto@gmail.com>
2020-07-10 15:17:16 -0300

    commit on master

--- Misc/CompoundInterestCalc/CompoundInterestCalculator.java
+++ Misc/CompoundInterestCalc/CompoundInterestCalculator.java
@@ -10,14 +10,13 @@ public class CompoundInterestCalculator {
     public CompoundInterestCalculator(String principal, String interest) {
         this.principal = new BigDecimal(principal);
         this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
-	}
-
+    }
 
     // TOTAL = PRINCIPAL*(1+INTEREST)^PERIODS
-	public BigDecimal calculateTotalValue(int noOfYears) {
+    public BigDecimal calculateTotalValue(int noOfYears) {
         BigDecimal onePlusInterest = interest.add(new BigDecimal(1));
         BigDecimal onePlusInterestPower = onePlusInterest.pow(noOfYears);
         BigDecimal totalValue = principal.multiply(onePlusInterestPower);
-            return totalValue;
-	}
+        return totalValue;
+    }
 }
--- Misc/CompoundInterestCalc/CompoundInterestCalculatorRunner.java
+++ Misc/CompoundInterestCalc/CompoundInterestCalculatorRunner.java
@@ -3,12 +3,10 @@ package Misc.CompoundInterestCalc;
 import java.math.BigDecimal;
 
 public class CompoundInterestCalculatorRunner {
-    
+
     public static void main(String[] args) {
-    CompoundInterestCalculator calculator =
-        new CompoundInterestCalculator("4500.00" , "7.5");
-    BigDecimal totalValue =
-        calculator.calculateTotalValue(5); // 5 years
-    System.out.println(totalValue);
+        CompoundInterestCalculator calculator = new CompoundInterestCalculator("4500.00", "7.5");
+        BigDecimal totalValue = calculator.calculateTotalValue(5); // 5 years
+        System.out.println(totalValue);
     }
 }

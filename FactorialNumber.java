45bf5326d149b1fd3f513c001d40b45dec7ec24f
Levi Preto <levi.preto@gmail.com>
2020-07-15 15:58:10 -0300

    commit on master

---
+++ FactorialNumber.java
@@ -0,0 +1,18 @@
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+
+        factorial(10);
+
+    }
+
+    private static void factorial(int number) {
+
+        int factorialCalculation = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorialCalculation = factorialCalculation * i;
+        }
+        System.out.println(factorialCalculation);
+    }
+}
---
+++ SumOfNumbersUpTo.java
@@ -0,0 +1,21 @@
+public class SumOfNumbersUpTo {
+    
+    public static void main(String[] args) {
+
+        sumUpTo(6);
+        
+    }
+
+
+    private static void sumUpTo(int max) {
+
+        int totalSum = 0;
+
+        for(int i = 1 ; i <= max ; i++) {
+            totalSum = totalSum + i;
+        }
+        System.out.println(totalSum);
+    }
+
+
+}

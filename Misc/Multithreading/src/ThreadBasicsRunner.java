2c0b35484543a477c8d377fb3517d318e9cc119d
Levi Preto <levi.preto@gmail.com>
2020-08-14 15:27:35 -0300

    commit to master

---
+++ Misc/Multithreading/src/ThreadBasicsRunner.java
@@ -0,0 +1,52 @@
+package Misc.Multithreading.src;
+
+class Task1 extends Thread {
+    public void run() {
+        System.out.println("\nTask 1 started ");
+
+        for (int i = 101; i <= 199; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nTask 1 done ");
+    }
+}
+
+class Task2 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("\nTask 2 started ");
+
+        for (int i = 201; i <= 299; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nTask 2 done ");
+    }
+}
+
+public class ThreadBasicsRunner {
+
+    public static void main(String[] args) {
+        // Task 1
+        System.out.println("\nTask1 Kicked Off");
+        Task1 task1 = new Task1();
+        task1.start();
+
+        // Task 2
+        System.out.println("\nTask2 Kicked Off");
+        Task2 task2 = new Task2();
+        Thread task2Thread = new Thread(task2);
+        task2Thread.start();
+
+        // Task 3
+        System.out.println("\nTask3 Kicked Off");
+        for (int i = 301; i <= 399; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nTask 3 done");
+        System.out.println("\nMain done");
+
+    }
+}

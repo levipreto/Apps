072ae8e5d9e95345af2f5684f423700f47a7dc7c
Levi Preto <levi.preto@gmail.com>
2020-08-19 14:06:47 -0300

    commit on master

---
+++ Misc/Concurrency/src/CopyOnWriteArrayListRunner.java
@@ -0,0 +1,23 @@
+package Misc.Concurrency.src;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListRunner {
+    
+    public static void main(String[] args) {
+        
+        List<String> list = new CopyOnWriteArrayList<>();
+        list.add("Ant");
+        list.add("Bat");
+        list.add("Cat");
+
+
+        for(String element:list) {
+            System.out.println(element);
+        }
+
+
+
+    }
+}
